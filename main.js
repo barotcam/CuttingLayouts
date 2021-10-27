b170954878c89f5164e4b8572eba977ec84fe167
Camille Barot <camille.barot@gmail.com>
2021-10-27 22:13:48 +0200

    Open file dialog

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 // main.js
 
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain, dialog } = require('electron')
 const path = require('path')
 
 function createWindow () {
   const mainWindow = new BrowserWindow({
+    webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false
+    },
     width: 800,
     height: 600
   })
   
+  mainWindow.webContents.openDevTools()
   mainWindow.removeMenu()
 
   mainWindow.loadFile('main/index.html')
@@ -17,12 +22,8 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
-
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
@@ -39,3 +40,9 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+// listen to application messages
+ipcMain.on('open-file-dialog', function (e) {
+    console.log( '[message received]', 'open-file-dialog' )
+    dialog.showOpenDialogSync(e.sender)
+})
